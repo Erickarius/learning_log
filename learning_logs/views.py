53c02c9b38bce94e2de130acd21aa0f9ba13dad6
Eryk <erykkurkowski.praca@gmail.com>
2023-05-15 18:54:10 +0200

    view function edit_entry()

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 def index(request):
@@ -55,3 +55,21 @@ def new_entry(request, topic_id):
 	#Wyświetlanie pustego formularza
 	context = {'topic': topic, 'form': form}
 	return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+	"""Edycja istniejącego wpisu."""
+	entry = Entry.objects.get(id=entry_id)
+	topic = entry.topic
+
+	if request.method != 'POST':
+		#Żądanie początkowe, wypełnienie forumlarza aktualną treścią wpisu.
+		form = EntryForm(instance=entry)
+	else:
+		#Przkazano dane za pomocą żądania POST, należy je przetworzyć.
+		form = EntryForm(instance=entry, data=request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('learning_logs:topic', topic.id=topic.id)
+
+	context = {'entry': entry, 'topic': topic, 'form': form}
+	return render(request, 'learning_logs/edit_entry.html', context)
