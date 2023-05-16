3f9e053ec144fde04600401d860f48da2a4f60d1
Eryk <erykkurkowski.praca@gmail.com>
2023-05-16 14:52:25 +0200

    Adding the register() view function and adding a template for the user registration page

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,12 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 urlpatterns = [
 	#Dołączenie domyślnych adresów URL uwierzytelniania.
 	path('', include('django.contrib.auth.urls')),
+	#Strona rejestracji.
+	path('register/', views.register, name='register'),
 ]
