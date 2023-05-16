772cdb4d97a2147211ae998409220e3b0ac8eee3
Eryk <erykkurkowski.praca@gmail.com>
2023-05-16 17:41:00 +0200

    access restriction in learning log application

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -15,6 +15,7 @@ def topics(request):
 	context = {'topics': topics}
 	return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
 	"""Wyświetla pojedynczy temat i wszystkie powiązane z nim wpisy."""
 	topic = Topic.objects.get(id=topic_id)
@@ -22,6 +23,7 @@ def topic(request, topic_id):
 	context = {'topic':topic, 'entries': entries}
 	return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
 	"""Dodaj nowy temat."""
 	if request.method != 'POST':
@@ -38,6 +40,7 @@ def new_topic(request):
 	context = {'form': form}
 	return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
 	"""Dodanie nowego wpisu dla określonego tematu."""
 	topic = Topic.objects.get(id=topic_id)
@@ -58,6 +61,7 @@ def new_entry(request, topic_id):
 	context = {'topic': topic, 'form': form}
 	return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
 	"""Edycja istniejącego wpisu."""
 	entry = Entry.objects.get(id=entry_id)
