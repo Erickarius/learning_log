007a31d58c1c08ebcecc9dc7e0de8d19adfaeae6
Eryk <erykkurkowski.praca@gmail.com>
2023-05-17 12:22:36 +0200

    Define the html header

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'learning_logs',
     'users',
 
+    #Aplikacje innych firm.
+    'bootstrap4',
+
     #Domyślne aplikacje Django
     'django.contrib.admin',
     'django.contrib.auth',
